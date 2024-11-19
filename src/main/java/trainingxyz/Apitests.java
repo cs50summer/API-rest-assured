6e0954d0e192f43de337f7bfd5fa4aceef2ac88b
cs50summer <sameenaresume@gmail.com>
2024-11-19 11:31:48 -0600

    Final test of teh Multipviatmin entry

--- src/main/java/trainingxyz/Apitests.java
+++ src/main/java/trainingxyz/Apitests.java
@@ -38,12 +38,14 @@ public class Apitests {
     @Test
     public void createProduct() {
         String endpoint = "http://localhost:80/api_testing/product/create.php";
-        String body = """
-                { "name":"Water Bottle",
-                  "description":"Blue in color . Holds 64oz of water",
-                  "price": 15,
-                  "category_id":3,
-                }""";
+        String body =""" 
+                 {
+                 "name": "Water Bottle",
+                 "description": "Blue in color . Holds 64oz of water",
+                 "price": 15,
+                 "category_id": 3,
+                 }
+                 """;
         var response = given().body(body).when().post(endpoint).then();
         response.log().body();
     }
@@ -201,5 +203,23 @@ public class Apitests {
         Product actualProduct= given().queryParam("id", "2").when().get(endpoint).as(Product.class);
 
         assertThat(actualProduct,samePropertyValuesAs(expectedProduct));
+    }
+
+    @Test
+    public void getMultiVitamin(){
+        String endpoint = "http://localhost:80/api_testing/product/read_one.php";
+        given().
+                queryParam("id", 18).
+                when().
+                get(endpoint).then().log().body().
+                assertThat().
+                statusCode(200).
+                header("Content-Type", equalTo("application/json")).
+                body("id", equalTo("18")).
+                body("name", equalTo("Multi-Vitamin (90 capsules)")).
+                body("description",equalTo("A daily dose of our Multi-Vitamins fulfills a day’s nutritional needs for over 12 vitamins and minerals.")).
+                body("price", equalTo("10.00")).
+                body("category_id",equalTo(4)).
+                body("category_name",equalTo("Supplements"));
     }
 }
